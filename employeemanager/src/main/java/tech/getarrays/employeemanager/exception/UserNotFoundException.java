708d71a46a367666c254fd3761aaca2de75feecb
nhitran26197 <nhitran26197@gmail.com>
2023-06-17 22:04:35 -0500

    backend done

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package tech.getarrays.employeemanager.exception;
+
+
+
+public class UserNotFoundException  extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
